fde4f48fca523aabf21c0e587a1e4db5a4a9be58
DUDCHENKO Oleksandr <ovdudchenko@gmail.com>
2015-11-02 23:53:25 +0200

    Added solution 2 based on regex usage

--- task2_awk/run_awk.sh
+++ task2_awk/run_awk.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#awk -f passwd_awk.sh /etc/passwd 
+echo "---=== Solution 1 ===---"
 awk -f script_awk.sh sensors readings
+echo "---=== Solution 2 ===---"
+awk -f script_awk2.sh sensors readings
